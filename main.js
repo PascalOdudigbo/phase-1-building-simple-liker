505224b4c6a7b60f01a334e901e597ee8107a43c
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-06-16 16:47:21 +0300

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,45 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errModal = document.getElementById("modal");
+errModal.className = "hidden";
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+  const likeButtons = document.querySelectorAll(".like-glyph");
+
+    function likeEvent (event) {
+      const heart = event.target;
+      mimicServerCall()
+      .then(() => {
+        if(heart.innerHTML === EMPTY_HEART){
+          heart.innerHTML === FULL_HEART;
+          heart.classList.add("activated-heart");
+        }
+        else{
+          heart.innerHTML === EMPTY_HEART;
+          heart.classList.remove("activated-heart");
+        }
+        
+      })
+      .catch((err)=>{
+        errModal.classList.remove("hidden");
+        modalErrMessage = document.getElementById("modal-message");
+        modalErrMessage.textContent = err.message;
+        setTimeout(() => {
+          errModal.classList.add("hidden");
+
+        }, 3000)
+
+      })
+  };
+ 
+  likeButtons.forEach((heart)=>{
+    heart.addEventListener("click", likeEvent)
+  })
+
+
+})
 
 
 
